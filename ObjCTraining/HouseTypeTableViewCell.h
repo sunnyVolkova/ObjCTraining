20388fe24017ee1d5dfed2722968dd7d658b1573
Alexandra Volkova <avolkova@weezlabs.com>
2016-03-23 15:24:04 +0300

    storyboard created

---
+++ ObjCTraining/HouseTypeTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  HouseTypeTableViewCell.h
+//  ObjCTraining
+//
+//  Created by RWuser on 23/03/16.
+//  Copyright © 2016 RWuser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HouseTypeTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *typeImage;
+@property (weak, nonatomic) IBOutlet UILabel *typeName;
+
+@end
---
+++ ObjCTraining/HouseTypeTableViewCell.m
@@ -0,0 +1,25 @@
+//
+//  HouseTypeTableViewCell.m
+//  ObjCTraining
+//
+//  Created by RWuser on 23/03/16.
+//  Copyright © 2016 RWuser. All rights reserved.
+//
+
+#import "HouseTypeTableViewCell.h"
+@interface HouseTypeTableViewCell ()
+
+@end
+@implementation HouseTypeTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
